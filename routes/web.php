e15b9dddefa9c47798947642711ce2b121bd8c7f
Felipe Fernandez <felipefernandezmorel1602@gmail.com>
2024-10-19 12:25:26 -0300

    Aprendiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Bienvenido a la pagina principal";
 });
+
+Route::get('/posts', function () {
+    return "Aqui se mostaran todos los posts";
+});
+
+Route::get('/posts/{post}', function ($post) { //Enrutado variable
+    return "Aqui se mostara el post {$post}";
+});
+
+//Get - Obtener pagina
+//Post - Publicar/Mandar informacion
+//Put/path - Actualizar registro
+//Delete - Borrar registro
