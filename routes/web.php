136c970d9d6179b71541a5d71dd99f9849d93575
Felipe Fernandez <felipefernandezmorel1602@gmail.com>
2024-10-19 17:09:49 -0300

    Enrutado de contenido variable

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,26 @@ Route::get('/posts', function () {
     return "Aqui se mostaran todos los posts";
 });
 
-Route::get('/posts/{post}', function ($post) { //Enrutado variable
+Route::get('/posts/create', function () { //Enrutado de contenido variable
+    return "Aqui se mostara un formulario para crear un post";
+});
+
+Route::get('/posts/{post}', function ($post) { //Enrutado de contenido variable
     return "Aqui se mostara el post {$post}";
 });
+
+#Route::get('/posts/{post}/{category}', function ($post,$category = null) { //Enrutado de contenido variable
+    
+#    if($category){
+#        return "Aqui se mostara el post {$post} de la categoria {$category}";
+#    }else{
+#        return "Aqui se mostara el post {$post}";
+#    }
+#});
+
+#Route::get('/posts/{post}/{category}', function ($post,$category) { //Enrutado de contenido variable
+#    return "Aqui se mostara el post {$post} de la categoria {$category}";
+#});
 
 //Get - Obtener pagina
 //Post - Publicar/Mandar informacion
