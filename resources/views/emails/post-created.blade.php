9d229fb72efc7b01cac49c204e1056b61b596945
Felipe Fernandez <felipefernandezmorel1602@gmail.com>
2024-10-23 14:44:18 -0300

    Markdowns on Laravel

--- resources/views/emails/post-created.blade.php
+++ resources/views/emails/post-created.blade.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        Correo por aprobar
-    </h1>
+<x-mail::message>
 
-    <p>
-        Se ha creado un nuevo post que necesita ser aprobado
-    </p>
+# Correo por aprobar
 
-    <a href="{{route('posts.show', $post)}}">
-        Click para aprobar
-    </a>
+<x-mail::panel>
+Se ha creado un nuevo post que necesita ser aprobado
+</x-mail::panel>
 
-</body>
-</html>
+<x-mail::button :url="route('posts.show', $post)" color="success">
+Click para aprobar
+</x-mail::button>
 
+</x-mail::message>
